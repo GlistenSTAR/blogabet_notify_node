20eb3c6e5cfaa86c241bf199d6c02cef23d37889
GlistenSTAR <glistenstar00@gmail.com>
2024-05-24 23:20:45 +0200

    updatE: finish the project

--- extension/background.js
+++ extension/background.js
@@ -37,20 +37,22 @@ const getNewEmail = async (tabId) => {
   await chrome.scripting.executeScript({
     target: { tabId: tabId },
     function: (intervalId) => {
-      let old_date;
+      let old_date, old_caption = '', old_url = '', url;
       const extractUrl = () => {
         let base_xpath = "#MailList > div > div > div > div > div > div > div > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div:nth-child(2)"
         let title = document.querySelector(`${base_xpath} > div:nth-child(1)`).innerText.trim()
         let new_date = document.querySelector(`${base_xpath} > div:nth-child(2) > span`).innerText.trim()
-        let caption =  document.querySelector(`${base_xpath} > div:nth-child(2) > div > span`).innerText.trim()
-        console.log(new_date, caption)
+        let caption = document.querySelector(`${base_xpath} > div:nth-child(2) > div > span`).innerText.trim()
+        try {
+          const urlRegex = /(https?:\/\/[^\s]+)/g;
+          url = document.querySelector(`${base_xpath} > div:nth-child(3)`).innerText.trim().match(urlRegex)[0]
+        } catch (err) { }
+        console.log(new_date)
         try {
           if (title == "Blogabet") {
-            if (new_date != old_date) {
-              old_date = new_date;
-              const urlRegex = /(https?:\/\/[^\s]+)/g;
-              let url = document.querySelector(`${base_xpath} > div:nth-child(3)`).innerText.trim().match(urlRegex)[0]
-              let caption =  document.querySelector(`${base_xpath} > div:nth-child(2) > div > span`).innerText.trim()
+            if (url != old_url) {
+              old_url = url
+              caption = document.querySelector(`${base_xpath} > div:nth-child(2) > div > span`).innerText.trim()
               console.log(url)
               fetch('http://127.0.0.1:5000/api/blogabet', {
                 method: 'POST',
