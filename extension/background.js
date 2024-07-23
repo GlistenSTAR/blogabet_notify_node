367b7fd74db418c4c1a8fedb25827b0dfd60693a
GlistenSTAR <glistenstar00@gmail.com>
2024-07-23 13:37:50 -0300

    update: extension for add proxy using option page

--- extension/background.js
+++ extension/background.js
@@ -1,35 +1,42 @@
 let isActive = false;
-let intervalId = "";
+let intervalId = '';
 
 chrome.commands.onCommand.addListener(async (command) => {
   let tab;
-  await chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-    tab = tabs[0]
-    if (command === "_execute_action") {
-      isActive = !isActive
-      if (isActive) {
-        chrome.action.setBadgeText({ tabId: tab.id, text: "O" });
-        await getNewEmail(tab.id)
-      } else {
-        try {
-          chrome.action.setBadgeText({ tabId: tab.id, text: "" });
-          clearInterval(intervalId)
-        } catch (err) { console.log(err) }
+  await chrome.tabs.query(
+    { active: true, currentWindow: true },
+    async (tabs) => {
+      tab = tabs[0];
+      if (command === '_execute_action') {
+        isActive = !isActive;
+        if (isActive) {
+          chrome.action.setBadgeText({ tabId: tab.id, text: 'O' });
+          await getNewEmail(tab.id);
+        } else {
+          try {
+            chrome.action.setBadgeText({ tabId: tab.id, text: '' });
+            clearInterval(intervalId);
+          } catch (err) {
+            console.log(err);
+          }
+        }
       }
     }
-  })
+  );
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  isActive = !isActive
+  isActive = !isActive;
   if (isActive) {
-    chrome.action.setBadgeText({ tabId: tab.id, text: "O" });
-    await getNewEmail(tab.id)
+    chrome.action.setBadgeText({ tabId: tab.id, text: 'O' });
+    await getNewEmail(tab.id);
   } else {
     try {
-      chrome.action.setBadgeText({ tabId: tab.id, text: "" });
-      clearInterval(intervalId)
-    } catch (err) { console.log(err) }
+      chrome.action.setBadgeText({ tabId: tab.id, text: '' });
+      clearInterval(intervalId);
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
 
@@ -37,46 +44,67 @@ const getNewEmail = async (tabId) => {
   await chrome.scripting.executeScript({
     target: { tabId: tabId },
     function: (intervalId) => {
-      let old_date, old_caption = '', old_url = '', url;
+      let old_date,
+        old_caption = '',
+        old_url = '',
+        url;
       const extractUrl = () => {
-        let base_xpath = "#MailList > div > div > div > div > div > div > div > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div:nth-child(2)"
-        let title = document.querySelector(`${base_xpath} > div:nth-child(1)`).innerText.trim()
-        let new_date = document.querySelector(`${base_xpath} > div:nth-child(2) > span`).innerText.trim()
-        let caption = document.querySelector(`${base_xpath} > div:nth-child(2) > div > span`).innerText.trim()
+        let base_xpath =
+          '#MailList > div > div > div > div > div > div > div > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div:nth-child(2)';
+        let title = document
+          .querySelector(`${base_xpath} > div:nth-child(1)`)
+          .innerText.trim();
+        let new_date = document
+          .querySelector(`${base_xpath} > div:nth-child(2) > span`)
+          .innerText.trim();
+        let caption = document
+          .querySelector(`${base_xpath} > div:nth-child(2) > div > span`)
+          .innerText.trim();
         try {
           const urlRegex = /(https?:\/\/[^\s]+)/g;
-          url = document.querySelector(`${base_xpath} > div:nth-child(3)`).innerText.trim().match(urlRegex)[0]
-        } catch (err) { }
-        console.log(new_date)
+          url = document
+            .querySelector(`${base_xpath} > div:nth-child(3)`)
+            .innerText.trim()
+            .match(urlRegex)[0];
+        } catch (err) {}
+        console.log(new_date);
         try {
-          if (title == "Blogabet") {
+          if (title == 'Blogabet') {
             if (url != old_url) {
-              old_url = url
-              caption = document.querySelector(`${base_xpath} > div:nth-child(2) > div > span`).innerText.trim()
-              console.log(url)
-              fetch('http://127.0.0.1:5000/api/blogabet', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                  url: url,
-                  caption: caption
+              old_url = url;
+              caption = document
+                .querySelector(`${base_xpath} > div:nth-child(2) > div > span`)
+                .innerText.trim();
+              console.log(url);
+              chrome.storage.sync.get(['proxy'], (items) => {
+                console.log({ url: url, caption: caption, proxy: items.proxy });
+                fetch('http://127.0.0.1:5000/api/blogabet', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    url: url,
+                    caption: caption,
+                    proxy: items.proxy,
+                  }),
                 })
-              })
-                .then(response => response.json())
-                .then(data => {
-                  console.log('send api', data);
-                })
-                .catch(error => {
-                  console.error('Error making Fetch POST request:', error);
-                });
+                  .then((response) => response.json())
+                  .then((data) => {
+                    console.log('send api', data);
+                  })
+                  .catch((error) => {
+                    console.error('Error making Fetch POST request:', error);
+                  });
+              });
             }
           }
-        } catch (err) { console.log(err) }
-      }
+        } catch (err) {
+          console.log(err);
+        }
+      };
       intervalId = setInterval(extractUrl, 1000);
     },
-    args: [intervalId]
-  })
-}
+    args: [intervalId],
+  });
+};
---
+++ extension/options.js
@@ -0,0 +1,14 @@
+document.getElementById('saveButton').addEventListener('click', () => {
+  const proxy = document.getElementById('proxy').value;
+
+  chrome.storage.sync.set({ proxy }, () => {
+    alert('Proxy saved.');
+  });
+});
+
+// Load options when the page is opened
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get(['proxy'], (items) => {
+    document.getElementById('proxy').value = items.proxy || '';
+  });
+});
