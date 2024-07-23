6ed21f06fea95bdc8de08358bbcf73c6bc93c007
GlistenSTAR <glistenstar00@gmail.com>
2024-07-23 13:38:16 -0300

    update: running the backend using sended proxy url

--- apis/blogabet.js
+++ apis/blogabet.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
 const axios = require('axios');
 const FormData = require('form-data');
 
-const Captcha = require("2captcha")
+const Captcha = require('2captcha');
 
-const browserObject = require("./../utils/browser");
-const delay = require("./../helper/delay");
+const browserObject = require('./../utils/browser');
+const delay = require('./../helper/delay');
 
 const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
 const isValidURL = (url) => {
   return urlPattern.test(url);
-}
+};
 
 dotenv.config();
 
@@ -23,54 +23,78 @@ dotenv.config();
 // capmonster
 // const { RecaptchaV2Task } = require("node-capmonster");
 
+router.get('/', (req, res) => {
+  res.json('test');
+});
 
-router.get('/', (req, res) => { res.json("test") })
-
-router.post("/", async (req, res) => {
-  let sitekey = process.env.siteKey
+router.post('/', async (req, res) => {
+  let sitekey = process.env.siteKey;
   let apiToken = process.env.telegramToken;
-  let chatId = process.env.telegramChatId
-  let capsoverApiKey = process.env.capsoverApiKey
+  let chatId = process.env.telegramChatId;
+  let capsoverApiKey = process.env.capsoverApiKey;
   let browser, page;
   let json = {
     chat_id: chatId,
     parse_mode: 'html',
-    text: `${req.body.caption}`
-  }
-  if (!req.body.caption.startsWith("FREE")) {
-    try {
-      await axios.post(`https://api.telegram.org/bot${apiToken}/sendMessage`, json)
-        .then(() => {
-          console.log("Telelgram message sent!")
-        })
-    } catch (err) {
-      console.log("Telegram message failed!")
-      console.log(err)
+    text: `${req.body.caption}`,
+  };
+  if (!req.body.caption.startsWith('FREE')) {
+    if (
+      req.body.caption.startsWith('New pick') ||
+      req.body.caption.startsWith('LIVE pick') ||
+      req.body.caption.startsWith('New Asian Odds pick') ||
+      req.body.caption.startsWith('LIVE Asian Odds pick') ||
+      req.body.caption.startsWith('New Combo pick')
+    ) {
+      try {
+        await axios
+          .post(`https://api.telegram.org/bot${apiToken}/sendMessage`, json)
+          .then(() => {
+            console.log('Telelgram message sent!');
+          });
+      } catch (err) {
+        console.log('Telegram message failed!');
+        console.log(err);
+      }
     }
   }
 
-  browser = await browserObject.startBrowser(false);
+  console.log(req.body.proxy);
+
+  browser = await browserObject.startBrowser(false, req.body.proxy);
   page = await browser.pages();
   page = page[0];
   await page.setDefaultNavigationTimeout(0);
   try {
-    let url = req.body.url
-    console.log(url)
+    let url = req.body.url;
+    console.log(url);
     if (!url || !isValidURL(url)) {
-      console.log("input url not specified");
+      console.log('input url not specified');
       await startAction();
-      return 0
+      return 0;
     }
 
     const startTime = performance.now();
     await page.goto(url);
-    await delay(200)
-
+    await delay(200);
 
     try {
-      await page.waitForSelector('div.g-recaptcha', { timeout: 1000 })
+      const bot_content = await page.$eval(
+        'body > div.container > div.block > h4',
+        (h4) => h4.innerText
+      );
+      if (bot_content.startsWith('Anti-bot')) {
+        try {
+          await page.waitForSelector('div.g-recaptcha', { timeout: 1000 });
+        } catch (err) {
+          await page.reload(url);
+          await page.waitForSelector('div.g-recaptcha', { timeout: 1000 });
+        }
+      } else {
+        throw new Error('No captcha!!!');
+      }
 
-      console.log("solving captcha...", sitekey, url)
+      console.log('solving captcha...', sitekey, url);
 
       // const solver = new Captcha.Solver(process.env.twocaptchaKey)
       // let token;
@@ -137,18 +161,20 @@ router.post("/", async (req, res) => {
       //     console.error(err.message)
       //   })
 
-
       // solve captcah using capsolver
       async function createTask() {
         try {
-          const response = await axios.post('https://api.capsolver.com/createTask', {
-            clientKey: capsoverApiKey,
-            task: {
-              type: 'NoCaptchaTaskProxyless',
-              websiteURL: url,
-              websiteKey: sitekey
+          const response = await axios.post(
+            'https://api.capsolver.com/createTask',
+            {
+              clientKey: capsoverApiKey,
+              task: {
+                type: 'NoCaptchaTaskProxyless',
+                websiteURL: url,
+                websiteKey: sitekey,
+              },
             }
-          });
+          );
 
           if (response.data.errorId !== 0) {
             throw new Error(`Error: ${response.data.errorDescription}`);
@@ -164,10 +190,13 @@ router.post("/", async (req, res) => {
       async function getTaskResult(taskId) {
         while (true) {
           try {
-            const response = await axios.post('https://api.capsolver.com/getTaskResult', {
-              clientKey: capsoverApiKey,
-              taskId: taskId
-            });
+            const response = await axios.post(
+              'https://api.capsolver.com/getTaskResult',
+              {
+                clientKey: capsoverApiKey,
+                taskId: taskId,
+              }
+            );
 
             if (response.data.errorId !== 0) {
               throw new Error(`Error: ${response.data.errorDescription}`);
@@ -181,7 +210,7 @@ router.post("/", async (req, res) => {
           }
 
           // Wait for 5 seconds before checking again
-          await new Promise(resolve => setTimeout(resolve, 5000));
+          await new Promise((resolve) => setTimeout(resolve, 5000));
         }
       }
 
@@ -190,61 +219,73 @@ router.post("/", async (req, res) => {
 
       const token = await getTaskResult(taskId);
 
-      await page.evaluate(
-        async (token) => {
-          document.getElementById("g-recaptcha-response").innerHTML = token;
-        },
-        token
-      );
+      await page.evaluate(async (token) => {
+        document.getElementById('g-recaptcha-response').innerHTML = token;
+      }, token);
 
-      await delay(200)
+      await delay(200);
       await page.evaluate((token) => {
         window.findRecaptchaClients = function () {
-          if (typeof (___grecaptcha_cfg) !== 'undefined') {
-            return Object.entries(___grecaptcha_cfg.clients).map(([cid, client]) => {
-              const data = { id: cid, version: cid >= 10000 ? 'V3' : 'V2' };
-              const objects = Object.entries(client).filter(([_, value]) => value && typeof value === 'object');
+          if (typeof ___grecaptcha_cfg !== 'undefined') {
+            return Object.entries(___grecaptcha_cfg.clients).map(
+              ([cid, client]) => {
+                const data = { id: cid, version: cid >= 10000 ? 'V3' : 'V2' };
+                const objects = Object.entries(client).filter(
+                  ([_, value]) => value && typeof value === 'object'
+                );
 
-              objects.forEach(([toplevelKey, toplevel]) => {
-                const found = Object.entries(toplevel).find(([_, value]) => (
-                  value && typeof value === 'object' && 'sitekey' in value && 'size' in value
-                ));
+                objects.forEach(([toplevelKey, toplevel]) => {
+                  const found = Object.entries(toplevel).find(
+                    ([_, value]) =>
+                      value &&
+                      typeof value === 'object' &&
+                      'sitekey' in value &&
+                      'size' in value
+                  );
 
-                if (typeof toplevel === 'object' && toplevel instanceof HTMLElement && toplevel['tagName'] === 'DIV') {
-                  data.pageurl = toplevel.baseURI;
-                }
-
-                if (found) {
-                  const [sublevelKey, sublevel] = found;
-
-                  data.sitekey = sublevel.sitekey;
-                  const callbackKey = data.version === 'V2' ? 'callback' : 'promise-callback';
-                  const callback = sublevel[callbackKey];
-                  data.topKey = toplevelKey;
-                  data.subKey = sublevelKey;
-                  if (!callback) {
-                    data.callback = null;
-                    data.function = null;
-                  } else {
-                    data.function = callback;
-                    const keys = [cid, toplevelKey, sublevelKey, callbackKey].map((key) => `['${key}']`).join('');
-                    data.callback = `___grecaptcha_cfg.clients${keys}`;
+                  if (
+                    typeof toplevel === 'object' &&
+                    toplevel instanceof HTMLElement &&
+                    toplevel['tagName'] === 'DIV'
+                  ) {
+                    data.pageurl = toplevel.baseURI;
                   }
-                }
-              });
-              return data;
-            });
+
+                  if (found) {
+                    const [sublevelKey, sublevel] = found;
+
+                    data.sitekey = sublevel.sitekey;
+                    const callbackKey =
+                      data.version === 'V2' ? 'callback' : 'promise-callback';
+                    const callback = sublevel[callbackKey];
+                    data.topKey = toplevelKey;
+                    data.subKey = sublevelKey;
+                    if (!callback) {
+                      data.callback = null;
+                      data.function = null;
+                    } else {
+                      data.function = callback;
+                      const keys = [cid, toplevelKey, sublevelKey, callbackKey]
+                        .map((key) => `['${key}']`)
+                        .join('');
+                      data.callback = `___grecaptcha_cfg.clients${keys}`;
+                    }
+                  }
+                });
+                return data;
+              }
+            );
           }
           return [];
-        }
+        };
 
         window.callbackRes = findRecaptchaClients();
-        let rTopKey = window.callbackRes[0].topKey
-        let rSubKey = window.callbackRes[0].subKey
-        window.___grecaptcha_cfg.clients[0][rTopKey][rSubKey]['callback'](token)
-
-      }, token)
-
+        let rTopKey = window.callbackRes[0].topKey;
+        let rSubKey = window.callbackRes[0].subKey;
+        window.___grecaptcha_cfg.clients[0][rTopKey][rSubKey]['callback'](
+          token
+        );
+      }, token);
 
       // // solve the captcha using anti-captcha
       // let token = await ac.solveRecaptchaV2Proxyless(url, sitekey);
@@ -318,35 +359,44 @@ router.post("/", async (req, res) => {
       //   window.___grecaptcha_cfg.clients[0][rTopKey][rSubKey]['callback'](token)
 
       // }, token)
+    } catch (err) {
+      console.log('there is no captcha', err);
+    }
 
-    } catch (err) { console.log("there is no captcha", err) }
-
-    await delay(2000)
-    await page.waitForSelector('div.feed-pick-title')
+    await delay(2000);
+    await page.waitForSelector('div.feed-pick-title');
     let eventName = await page.title();
-    eventName = eventName.split(" -")[0]
-    let title = await page.$eval('div.feed-pick-title > div.no-padding > h3', h3 => h3.innerText)
-    let content1 = '', content2 = '', content3 = '';
+    eventName = eventName.split(' -')[0];
+    let title = await page.$eval(
+      'div.feed-pick-title > div.no-padding > h3',
+      (h3) => h3.innerText
+    );
+    let content1 = '',
+      content2 = '',
+      content3 = '';
 
     try {
-      content1 = await page.$eval('div.pick-line', div => div.innerText)
-    } catch (err) { }
+      content1 = await page.$eval('div.pick-line', (div) => div.innerText);
+    } catch (err) {}
     try {
-      content2 = await page.$eval('div.sport-line', div => div.innerText)
+      content2 = await page.$eval('div.sport-line', (div) => div.innerText);
     } catch (err) {
       try {
-        let trs = await page.$$('table.table.combo-table > tbody > tr')
+        let trs = await page.$$('table.table.combo-table > tbody > tr');
         for (var i = 2; i < trs.length + 1; i++) {
-          content2 += await page.$eval(`table.table.combo-table > tbody > tr:nth-child(${i})`, div => div.innerText.trim().replace(/\s{2,}/g, " "))
-          content2 = content2 + "\n\n"
+          content2 += await page.$eval(
+            `table.table.combo-table > tbody > tr:nth-child(${i})`,
+            (div) => div.innerText.trim().replace(/\s{2,}/g, ' ')
+          );
+          content2 = content2 + '\n\n';
         }
-      } catch (err) { }
+      } catch (err) {}
     }
     try {
-      content3 = await page.$eval('div.labels', div => div.innerText)
-    } catch (err) { }
+      content3 = await page.$eval('div.labels', (div) => div.innerText);
+    } catch (err) {}
 
-    browser.close()
+    browser.close();
 
     // add screenshot for bet
     // const contentBoundingBox = await page.$eval('#feed-list', element => {
@@ -384,35 +434,39 @@ router.post("/", async (req, res) => {
     let json1 = {
       chat_id: chatId,
       parse_mode: 'html',
-      text: `Solving captcha time: ${runningTime / 1000}s`
-    }
+      text: `Solving captcha time: ${runningTime / 1000}s`,
+    };
 
     try {
-      await axios.post(`https://api.telegram.org/bot${apiToken}/sendMessage`, json1)
+      await axios
+        .post(`https://api.telegram.org/bot${apiToken}/sendMessage`, json1)
         .then(() => {
-          console.log("Telelgram message sent!")
-        })
+          console.log('Telelgram message sent!');
+        });
     } catch (err) {
-      console.log("Telegram message failed!")
-      console.log(err)
+      console.log('Telegram message failed!');
+      console.log(err);
     }
 
     let json = {
       chat_id: chatId,
       parse_mode: 'html',
-      text: `${eventName}\n\n${title}\n${content1}\n\n${content2}\n\n${content3}`
-    }
+      text: `${eventName}\n\n${title}\n${content1}\n\n${content2}\n\n${content3}`,
+    };
 
     try {
-      await axios.post(`https://api.telegram.org/bot${apiToken}/sendMessage`, json)
+      await axios
+        .post(`https://api.telegram.org/bot${apiToken}/sendMessage`, json)
         .then(() => {
-          console.log("Telelgram message sent!")
-        })
+          console.log('Telelgram message sent!');
+        });
     } catch (err) {
-      console.log("Telegram message failed!")
-      console.log(err)
+      console.log('Telegram message failed!');
+      console.log(err);
     }
-  } catch (err) { console.log("err>>>>>>", err) }
+  } catch (err) {
+    console.log('err>>>>>>', err);
+  }
 
   res.json(1);
 });
--- utils/browser.js
+++ utils/browser.js
@@ -7,13 +7,12 @@ const proxy_rotation = require('./proxyRotation');
 const proxyChain = require('proxy-chain');
 const delay = require('../helper/delay');
 
-const startBrowser = async (headless = 'new') => {
+const startBrowser = async (headless = 'new', proxy) => {
   let browser;
   try {
     console.log('Opening the browser......');
-    let proxy = await proxy_rotation(proxy_data);
-    const [host, port, username, password] = proxy.split(':');
-    const originalUrl = `http://${username}:${password}@${host}:${port}`;
+    const [host, port] = proxy.split(':');
+    const originalUrl = `http://EbRCJFtt:MbsZ2tmF@${host}:${port}`;
     const newUrl = await proxyChain.anonymizeProxy(originalUrl);
     await delay(1000);
 
